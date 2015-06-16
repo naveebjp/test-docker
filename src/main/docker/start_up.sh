1a28c4b9df51453a9447181a21a3b5fb528da5e9
M1018434 <M1018434@A2MD21054.mindtree.com>
2015-06-16 12:47:27 +0530

    ddd

--- src/main/docker/start_up.sh
+++
@@ -1,9 +0,0 @@
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-	<Context><WatchedResource>WEB-INF/web.xml</WatchedResource>
-	<Environment name=\"location\" value=\"https://s3.amazonaws.com/vikesh-vehicle-rental/frame.css\" type=\"java.lang.String\"/>
-	<Environment name=\"datepickercsslocation\" value=\"https://s3.amazonaws.com/vikesh-vehicle-rental/jsDatePick_ltr.min.css\" type=\"java.lang.String\"/>
-	</Context>"  > /etc/tomcat7/context.xml ;
-
-cat /etc/tomcat7/context.xml;
-service tomcat7 start ;
-tail -f /var/lib/tomcat7/logs/catalina.out
